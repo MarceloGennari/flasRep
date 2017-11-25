2e69d08fbb8026d6f09fa7e2cb564b1e70dce4c2
Walter Goodwin <bibbygoodwin@gmail.com>
2017-11-25 15:16:58 +0000

    GetPose altered - gets eye info too, etc

--- Read_Cog.py
+++ Read_Cog.py
@@ -1,18 +1,19 @@
 import pickle
 from lib.Face import Attribute
-import sys
-sys.dont_write_bytecode=True
 
-filename = 'faces/facedetect'
+filename = 'faces/facessleeping'
 
-fileObject = open(filename, 'r')
+fileObject = open(filename, 'rb')
 x = pickle.load(fileObject)
 
+# extract just the first detected face in the image - how can we be sure that this is the driver?
 y = x[0]["faceAttributes"]
+# print("y is %s" % y)
 
-print(x)
+# print("x is %s" % x)
 att = Attribute(x[0]["faceAttributes"])
-print(att.emotion)
+print(att.head_pose)
+
 
 # Possible emotions are:
 # neutral
