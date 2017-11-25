57ab2f8c4b4b547f7eb9cc08f266adcd708af93b
Marcelo Gennari <marcelogennari@outlook.com>
2017-11-25 22:32:19 +0000

    Added sound

--- static/record.js
+++ static/record.js
@@ -8,30 +8,39 @@ var video = document.querySelector('video');
 var cameraStream = '';
 var canvas= document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
+var send = true;
 
 function upload() {
   var dt = canvas.toDataURL('image/jpeg');
   dt = dt.substr(23);
-  $.ajax({
-    type: "POST",
-    url: "/video",
-    data: { 
-      imgBase64: dt
-    }
-  }).done(function(o) {
-    console.log('saved');
-    console.log(o);
-    $('#output').html(o);
-    if(o==":)"){
-      $('#idCan').css("border", "5px solid green");
-    }
-    if(o==":/"){
-      $('#idCan').css("border", "5px solid white");
-    }
-    if(o==":("){
-      $('#idCan').css("border", "5px solid red");
-    }
-  });
+    $.ajax({
+      type: "POST",
+      url: "/video",
+      data: { 
+        imgBase64: dt
+      }
+    }).done(function(o) {
+      console.log('saved');
+      console.log(o);
+      $('#output').html(o);
+      if(o==":)"){
+        $('#idCan').css("border", "5px solid green");
+      }
+      if(o==":/"){
+        $('#idCan').css("border", "5px solid white");
+      }
+      if(o==":("){
+        $('#idCan').css("border", "5px solid red");
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', 'static/audios/Sad1.mp3');
+        
+        audioElement.load()
+        audioElement.addEventListener("load", function() {
+          audioElement.play();
+        }, true);
+        
+      }
+    });
 };
 
 function drawFromVideo(){
@@ -48,8 +57,10 @@ if(navigator.getUserMedia){
       video.src = window.URL.createObjectURL(stream);
 
       setInterval(function() {
-        drawFromVideo();
-        upload();
+        if(send){
+          drawFromVideo();
+          upload();
+        }
       }, 5000);
     },
     function(){
@@ -64,6 +75,6 @@ document.querySelector('#stopbt').addEventListener(
   'click',
   function(e){
     video.src='';
-    cameraStream.stop();
+    send = false;
   }
 );
