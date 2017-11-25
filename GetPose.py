a670d7a8ac422a0b4972dcb3cde8d0b20f54dcfc
Walter Goodwin <bibbygoodwin@gmail.com>
2017-11-25 13:42:07 +0000

    More changes to getpose - removed need for intermediary file

--- GetPose.py
+++ GetPose.py
@@ -14,42 +14,22 @@ def getposefromimage(imagefile):
 
     #Image URL - Local Image or Online Imaged
     img_url = imagefile
-    filename = 'faces/facessleeping'
-    fileObject = open(filename, 'wb')
 
-
-    attributes = (
-                    'headPose'
-    )
+    # only query for the pose attribute, to avoid excessive info/(+delay?)
+    attributes = ('headPose')
 
     check = os.path.isfile('filename')
 
     if not check:
         result = CF.face.detect(img_url, False, False, attributes=attributes)
         print(result)
-        y = x[0]["faceAttributes"]
-        print("y is %s" % s)
-        pickle.dump(result, fileObject)
-
-    fileObject.close()
-
-    from lib.Face import Attribute
-
-
-def getpose(filename):
-    fileObject = open(filename, 'rb')
-    x = pickle.load(fileObject)
-
-    # extract just the first detected face in the image - how can we be sure that this is the driver?
-    y = x[0]["faceAttributes"]
-
-    pose = {"Pitch":y['headPose']['pitch'],"Roll":y['headPose']['roll'],"Yaw":y['headPose']['yaw']}
-    print(pose["Pitch"])
+        # extract just the first detected face in the image - how can we be sure that this is the driver?
+        y = result[0]["faceAttributes"]
+        # extract just the pose from this
+        pose = {"Pitch": y['headPose']['pitch'], "Roll": y['headPose']['roll'], "Yaw": y['headPose']['yaw']}
+        print("Roll is %d" % pose["Roll"])
 
     return pose
 
-
-getposefromimage(imagefile = '/Users/Bibby/Desktop/hackathon_images/sleep1.jpg')
-Pose = getpose(filename = 'faces/facessleeping')
-
-print(Pose)
+i = 1
+getposefromimage(imagefile = '/Users/Bibby/Desktop/hackathon_images/sleep%d.jpg' % i)
