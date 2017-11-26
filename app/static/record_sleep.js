0102f3222312353525e1782df02a64012ed6d150
Marcelo Gennari <marcelogennari@outlook.com>
2017-11-26 10:09:48 +0000

    Added Sound

--- app/static/record_sleep.js
+++ app/static/record_sleep.js
@@ -22,25 +22,16 @@ function upload() {
         imgBase64: dt
       }
     }).done(function(o) {
-      console.log('saved');
       console.log(o);
       $('#output').html(o);
-      if(o==":)"){
+      if(o=="awake"){
         $('#idCan').css("border", "5px solid green");
       }
-      if(o==":/"){
-        $('#idCan').css("border", "5px solid white");
-      }
-      if(o==":("){
+      if(o=="awake"){
         $('#idCan').css("border", "5px solid red");
-        var audioElement = document.createElement('audio');
-        audioElement.setAttribute('src', 'static/audios/Sad1.mp3');
-        
-        audioElement.load()
-        audioElement.addEventListener("load", function() {
-          audioElement.play();
-        }, true);
-        
+        $('#aud').attr("src", 'static/audios/test1.wav');
+        $('#aud').get(0).load();
+        $('#aud').get(0).play();
       }
     });
     numberScreenshots = numberScreenshots+1;
